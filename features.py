8605c889c52631ef809ffa2e3c4dcf15b16da6fb
VYSHNAVGITHUB <121009230+VYSHNAVGITHUB@users.noreply.github.com>
2023-07-28 17:07:32 +0530

    Add files via upload

--- features.py
+++ features.py
@@ -1,6 +1,7 @@
-#  doesn't keep order
-#  heterogeneous   (store multiple type datas)
-#  doesn't support duplication
-#  doesn't support indexing,slicing
-#  mutable   (can be updated)
-#  nesting not possible
+#   keep order
+#   heterogeneous
+#   support duplication
+#   support indexing
+#   support nesting
+#   immutable  (cannot update)
+
